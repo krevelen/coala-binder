800f470e88433fb5da0bc3d53b74c7aadaf7e61d
Rick van Krevelen <krevelen@gmail.com>
2016-10-28 15:00:14 +0200

    - refactored io.coala.random, including tests - fixed PseudoRandom#nextBigDecimal to draw number in [0,1] range - introduced LocalBinding tag interface for enterprise Actor

--- coala-api/src/main/java/io/coala/bind/LocalBinding.java
+++ coala-api/src/main/java/io/coala/bind/LocalBinding.java
@@ -1,7 +1,5 @@
 /* $Id$
  * 
- * Part of ZonMW project no. 50-53000-98-156
- * 
  * @license
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
@@ -15,32 +13,18 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  * 
- * Copyright (c) 2016 RIVM National Institute for Health and Environment 
+ * Copyright (c) 2016 
  */
-package io.coala.random;
-
-import java.lang.reflect.Method;
-
-import org.aeonbits.owner.Config;
-import org.aeonbits.owner.Converter;
+package io.coala.bind;
 
 /**
- * {@link DistributionConverter} utility for {@link Config}-interfaces
+ * {@link LocalBinding}
  * 
- * @param <T>
  * @version $Id$
  * @author Rick van Krevelen
  */
-public class DistributionConverter<T>
-	implements Converter<DistributionParsable<T>>
+public interface LocalBinding
 {
-	@Override
-	public DistributionParsable<T> convert( final Method method,
-		final String input )
-	{
-		return ( p, t ) ->
-		{
-			return p.parse( input, t );
-		};
-	}
+
+	LocalBinder binder();
 }
