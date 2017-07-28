2278c8f1de1250c61d29eafef8fc36f0da9cf639
Rick van Krevelen <krevelen@gmail.com>
2017-07-28 10:11:52 +0200

    fixed mis-merge

--- adapters/eve3-coala-adapter/src/main/java/io/coala/eve3/Eve3Invoker.java
+++ adapters/eve3-coala-adapter/src/main/java/io/coala/eve3/Eve3Invoker.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* $Id: 9b29d6b5d0ccbad74c81ea92abc2e5ff6ec760a3 $
  * 
  * Part of ZonMW project no. 50-53000-98-156
@@ -56,63 +55,4 @@ public class Eve3Invoker implements Invoker
 					subscriber );
 		} );
 	}
-=======
-/* $Id: 9b29d6b5d0ccbad74c81ea92abc2e5ff6ec760a3 $
- * 
- * Part of ZonMW project no. 50-53000-98-156
- * 
- * @license
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- * 
- * Copyright (c) 2016 RIVM National Institute for Health and Environment 
- */
-package io.coala.eve3;
-
-import java.lang.reflect.Method;
-import java.net.URI;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import io.coala.bind.LocalBinder;
-import io.coala.inter.Invoker;
-import io.reactivex.Observable;
-
-/**
- * {@link Eve3Invoker}
- * 
- * @version $Id: 9b29d6b5d0ccbad74c81ea92abc2e5ff6ec760a3 $
- * @author Rick van Krevelen
- */
-@Singleton
-public class Eve3Invoker implements Invoker
-{
-	@Inject
-	private LocalBinder binder;
-
-	@Inject
-	private Eve3Factory eve3;
-
-//	@SuppressWarnings( "unchecked" )
-	@Override
-	public <T> Observable<T> invoke( final URI target, final Method method,
-		final Object... args )
-	{
-		return Observable.unsafeCreate( subscriber ->
-		{
-			this.eve3.getAgent( this.binder.id() ).call( target, method, args,
-					subscriber );
-		} );
-	}
->>>>>>> branch 'develop' of https://github.com/krevelen/coala-binder.git
 }
--- coala-common/src/main/java/io/coala/config/JsonConverter.java
+++ coala-common/src/main/java/io/coala/config/JsonConverter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* $Id: 8fad8f633ea3b7ddc70ba233b570d9d510a017e9 $
  * 
  * Part of ZonMW project no. 50-53000-98-156
@@ -49,55 +48,3 @@ public class JsonConverter implements Converter<Object>
 		return JsonUtil.valueOf( /* "\"" + */ input /* + "\"" */, method.getReturnType() );
 	}
 }
-=======
-/* $Id: 8fad8f633ea3b7ddc70ba233b570d9d510a017e9 $
- * 
- * Part of ZonMW project no. 50-53000-98-156
- * 
- * @license
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- * 
- * Copyright (c) 2016 RIVM National Institute for Health and Environment 
- */
-package io.coala.config;
-
-import java.lang.reflect.Method;
-
-import javax.servlet.jsp.jstl.core.Config;
-
-import org.aeonbits.owner.Converter;
-
-import com.fasterxml.jackson.core.TreeNode;
-
-import io.coala.json.JsonUtil;
-
-/**
- * {@link JsonConverter} is a {@link Config} converter for {@link TreeNode}
- * values
- * 
- * @version $Id: 8fad8f633ea3b7ddc70ba233b570d9d510a017e9 $
- * @author Rick van Krevelen
- */
-public class JsonConverter implements Converter<Object>
-{
-//	@SuppressWarnings( "unchecked" )
-	@Override
-	public Object convert( final Method method, final String input )
-	{
-//		final Class<?> returnType = (Class<T>) TypeArguments
-//				.of( JsonConverter.class, getClass() ).get( 0 );
-//		Objects.requireNonNull( returnType );
-		return JsonUtil.valueOf( /* "\"" + */ input /* + "\"" */, method.getReturnType() );
-	}
-}
->>>>>>> branch 'develop' of https://github.com/krevelen/coala-binder.git
